cb9a9ccd52c87166dcb60d1c6466f29e4320879d
Vitor Ryan da Rosa <vitorryanrosaaa@hotmail.com>
2023-10-24 22:50:26 -0300

    sei la

--- source/JS/script.js
+++ source/JS/script.js
@@ -1,4 +1,9 @@
 function pesquisar() {
-    var nomea = document.getElementById('nomea')
+    var anome = window.document.getElementById('nomea')
+    var img = document.createElement('img')
+    img.setAttribute('id', 'animalf')
+    if (anome = vaca) {
+        img.setAttribute('src', '')
+    }
 
 }
